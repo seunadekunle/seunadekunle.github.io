4e734ffe0984040fa2e2960cae7d1272f93eb605
Seun Adekunle <seun9008@gmail.com>
2022-12-23 11:06:02 -0500

    Updates

---
+++ precache-manifest.f1efb788dce807552f12e923e292f8d1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4162b5ce923c3fbb3df33d01fcd08780",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83c4133571057abfef40",
+    "url": "/static/css/2.0e31c611.chunk.css"
+  },
+  {
+    "revision": "acffca0592b670fc15e5",
+    "url": "/static/css/main.2d9997d2.chunk.css"
+  },
+  {
+    "revision": "83c4133571057abfef40",
+    "url": "/static/js/2.1b5a9f31.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1b5a9f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acffca0592b670fc15e5",
+    "url": "/static/js/main.0b54e69a.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "16903388f6a9e3c6976a60990d7dd8ed",
+    "url": "/static/media/download.16903388.png"
+  },
+  {
+    "revision": "25fbff9a310b7bd61dba5e050b280740",
+    "url": "/static/media/resume.25fbff9a.pdf"
+  }
+]);
